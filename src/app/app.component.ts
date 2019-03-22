3edcf9e8b2b6ec0443a57d6106df155a56974f49
saudahabib <saudababs00@gmail.com>
2019-03-22 17:30:03 +0300

    Added initial quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  quotes: string[];
-  constructor(){
-    this.quotes= ["He know nothin","You know nothing Jon snow","I know nothing"]
+  quotes= [
+    new Quote(1,"You know nothing","Jon Snow","Sauda"),
+    new Quote(2,"A man has many faces","Valar Morghulis","James"),
+    new Quote(3,"Tell Cersei, I want her to know it was me","Lady Margaery","Kevin"),
+  ]
   }
-
-}
---
+++ src/app/quote.ts
@@ -0,0 +1,8 @@
+export class Quote {
+  constructor(
+    public id: number,
+    public quote: string,
+    public author: string,
+    public submitter: string
+  ){}
+}
